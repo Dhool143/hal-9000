be270a8ae468a4ff857c28c87551fea38d7b5dbb
Fathiya Yoosef <155487698+Dhool143@users.noreply.github.com>
2025-11-26 18:06:24 +0000

    add hall programe

---
+++ hal-9000.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#A  script to demo user input and  variabel use
+
+
+echo "Hello. I'M Hal 9000. What is your name?"
+
+read USERNAME 
+
+echo "I'M sorry? $USERNAME. I'M afraid I can't open the pod pay doors"
